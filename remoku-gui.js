2387e09329ed3080df9cf7d8648642bbc90139c6
gonzotek@gmail.com <gonzotek@gmail.com@ab0ec3bd-c85b-b755-42fc-a4ff9edab1fc>
2012-08-14 22:27:36 +0000

    add cur to long-cached filetypes list add try/catch to colorpicker call

--- remoku-gui.js
+++ remoku-gui.js
@@ -1696,26 +1696,30 @@ window.onload = function(){
     changeTextColor('.active', '#' + activeNavTextColor);
     changeTextColor('.bgcolor', '#' + txtcolor);
     changeTextColor('#reloadlink', '#' + txtcolor);
-    cP = colorPicker;
-    cP.exportColor = function () {
-	    bgcolor = bgcolorInput.value;
-			changeBackgroundColor('.bgcolor', '#' + bgcolor);
-			txtcolor = Brightness( bgcolor ) < 130 ? 'FFFFFF' : '000000';
-			changeTextColor('.bgcolor', '#' + txtcolor);
-	    navTextColor = Brightness( bgcolor ) < 130 ? 'D0D0D0' : '555555';
-	    activeNavTextColor = Brightness( bgcolor ) < 130 ? 'FFFFFF' : '000000';
-	    changeTextColor('.nav', '#' + navTextColor);
-	    changeTextColor('.active', '#' + activeNavTextColor);
-			setConfig('bgColor', bgcolor);
-			
-			fgcolor = fgcolorInput.value;
-	    txtcolor = Brightness( fgcolor ) < 130 ? 'FFFFFF' : '000000';
-	    for (var i = 0; i<fgElements.length;i++) {
-		    changeBackgroundColor(fgElements[i], '#' + fgcolor);
-			changeTextColor(fgElements[i], '#' + txtcolor);
-		    }
-		setConfig('fgColor', fgcolor);
-	  };
+		try {
+			cP = colorPicker;
+	    cP.exportColor = function () {
+		    bgcolor = bgcolorInput.value;
+				changeBackgroundColor('.bgcolor', '#' + bgcolor);
+				txtcolor = Brightness( bgcolor ) < 130 ? 'FFFFFF' : '000000';
+				changeTextColor('.bgcolor', '#' + txtcolor);
+		    navTextColor = Brightness( bgcolor ) < 130 ? 'D0D0D0' : '555555';
+		    activeNavTextColor = Brightness( bgcolor ) < 130 ? 'FFFFFF' : '000000';
+		    changeTextColor('.nav', '#' + navTextColor);
+		    changeTextColor('.active', '#' + activeNavTextColor);
+				setConfig('bgColor', bgcolor);
+				
+				fgcolor = fgcolorInput.value;
+		    txtcolor = Brightness( fgcolor ) < 130 ? 'FFFFFF' : '000000';
+		    for (var i = 0; i<fgElements.length;i++) {
+			    changeBackgroundColor(fgElements[i], '#' + fgcolor);
+				changeTextColor(fgElements[i], '#' + txtcolor);
+			    }
+			setConfig('fgColor', fgcolor);
+		  } catch (e){
+			  dbg(e);
+			  };
+  	}
 	    
     bgcolorInput.onfocus = function(){
 	    textModeOff();
